e6d26d708916306a13809d427363bc4583c7076e
Cristián Jiménez <cristian@cjconsultora.cl>
2026-04-21 19:36:49 -0400

    Cambios exclusivo de ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -67,3 +67,14 @@ def jugar_ahorcado():
 
 if __name__ == "__main__":
     jugar_ahorcado()
+
+# este archivo lo he cambiado para subirlo a git hub
+
+"""
+kdjhaksjdha
+
+jshAKJSHA
+
+skjahKJHA
+
+"""
